bd53244891ee80037ae5ea5ee5a2a7a1ff2c587d
@joaocordeiro <joaocordeiroalmeida01@gmail.com>
2023-05-03 23:30:45 -0300

    images playlist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        sfBlackLight: '#121212',
-        sfBlack: '#000000',
-        sfPurpleDark: '#1d0e42',
+        sfBlackLight: "#121212",
+        sfBlack: "#000000",
+        sfGrey: "#3a3a3a",
+        sfPurpleDark: "#1d0e42",
       },
     },
   },
